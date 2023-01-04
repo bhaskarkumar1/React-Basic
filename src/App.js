7432dac7a913053b095bd21cb3bcec298c4c961a
Bhaskar Kumar <bhaskarkumar.india@gmail.com>
2023-01-04 23:16:09 +0530

    inStyling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import "./styles.css";
 
+let greet="";
+let color="";
+const d=new Date();
+const hours=d.getHours();
+if(hours<12){
+  greet= "GoodMorning";
+  color="green";
+} 
+else if(hours>12 && hours<18){
+  greet="Good AfterNoon";
+} 
+else {
+  greet="Good Night";
+  color="blue";
+}
 function App() {
   return (
     <div> 
-    
-     <h1>Hello Star Boy!!!!!!!!</h1>
-    <ul>
-      <li>the weeknd</li>
-      <li>eminem</li>
-      <li>fotty-seven</li>
-    </ul>
+   
+     <h1 className="heading" style={{color:{color}}}>{greet}</h1>
   
      </div>
   
